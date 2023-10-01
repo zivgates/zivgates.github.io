6bcb6b2835c7ee23dcf3dbdaeb8dee107d8caaed
zvqle <103849459+realzvqle@users.noreply.github.com>
2023-09-30 19:53:59 -0500

    Add files via upload

---
+++ homepage.js
@@ -0,0 +1,29 @@
+console.log("Hello from scripti.js!");
+
+function about() {
+    document.getElementById("text1").textContent = "Hello, Welcome To Zivgates";
+    document.getElementById("text2").textContent = "This is the homepage, As I'm Planning to add more pages to the website in the future.";
+    document.getElementById("text3").textContent = "This is the direct replacement to zvqle's realm, so this will be my main focus from now on.";
+    document.getElementById("text4").textContent = "I have large plans for this website, so make sure to follow my github to see more!";
+    document.getElementById("text5").textContent = "";
+    // Your code for the "about" button functionality goes here
+}
+
+function aboutMe() {
+    document.getElementById("text1").textContent = "Hello, I'm zvqle (not revealing my real name 😉).";
+    document.getElementById("text2").textContent = "I'm Mainly a C/C++ Programmer, But I also enjoy making Frontend For Websites.";
+    document.getElementById("text3").textContent = "I also enjoy talking politics, and random geography stuff.";
+    document.getElementById("text4").textContent = "Visit My Twitter, Github, And Youtube To see What I Make.";
+    document.getElementById("text5").textContent = "🇸🇴/🇺🇸";
+    // Your code for the "about" button functionality goes here
+}
+
+function clearContent() {
+    document.getElementById("text1").textContent = "";
+    document.getElementById("text2").textContent = "";
+    document.getElementById("text3").textContent = "";
+    document.getElementById("text4").textContent = "";
+    document.getElementById("text5").textContent = "";
+}
+
+
